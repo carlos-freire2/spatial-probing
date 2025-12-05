15f484119fecffaabf183ff5a6a0bc1f33ac3d8f
Madeleine Fenner <madeleinefenner@Madeleines-MacBook-Pro.local>
2025-12-05 11:49:34 -0500

    feat: add script to preprocess RAVEN data

---
+++ scripts/preprocess_RAVEN.py
@@ -0,0 +1,101 @@
+"""
+Preprocesses RAVEN dataset by stitching puzzle images into a grid.
+
+Example: 
+python scripts/preprocess_RAVEN.py \
+    --data_dir /data/RAVEN-10000/ \
+    --save_dir /data/RAVEN_preprocessed/
+"""
+
+import os
+import argparse
+import numpy as np
+import json
+import xml.etree.ElementTree as ET
+
+def main():
+    parser = argparse.ArgumentParser(description="Preprocess RAVEN dataset")
+    parser.add_argument("--data_dir", help="path to directory where raw data files are located")
+    parser.add_argument("--save_dir", help="path to directory where processed data will be saved")
+    args = parser.parse_args()
+
+    dirs = [d.name for d in os.scandir(args.data_dir) if d.is_dir()]
+    for type_dir in dirs:
+
+        images = []
+        labels = {}
+
+        type_dir_path = os.path.join(args.data_dir, type_dir)
+        filenames = os.listdir(type_dir_path)
+        for f in filenames:
+            if f.endswith(".xml"): continue
+
+            npz_path = os.path.join(args.data_dir, type_dir, f)
+            data = np.load(npz_path)
+            grid = stitch_grid(data["image"])
+            images.append(grid)
+
+            num = f[f.find("_")+1:f.rfind("_")]
+            split = f[f.rfind("_")+1:f.rfind(".")]
+
+            xml_data_filename = f[:f.rfind(".")] + ".xml"
+            xml_path = os.path.join(args.data_dir, type_dir, xml_data_filename)
+            tree = ET.parse(xml_path)
+            root = tree.getroot()
+
+            rules_data = get_rules(root)
+
+            labels[num] = {
+                "split": split,
+                "rules": rules_data,
+                "filename": f
+            }
+
+        save_path = os.path.join(args.save_dir, type_dir)
+        os.makedirs(save_path, exist_ok=True)
+
+        images = np.array(images)
+        image_save_path = os.path.join(args.save_dir, type_dir, "images.npy")
+        np.save(image_save_path, images)
+
+        labels_save_path = os.path.join(args.save_dir, type_dir, "labels.json")
+        with open(labels_save_path, "w") as f:
+            json.dump(labels, f, indent=4)
+        
+def get_rules(root):
+    rules_element = root.find("Rules")
+    rules_data = {}
+    for rule_group in rules_element.findall("Rule_Group"):
+        r = {}
+        rule_count = 0
+        for rule in rule_group.findall("Rule"):
+            r["rule_" + str(rule_count)] = {
+                "name": rule.get("name"),
+                "attribute": rule.get("attr")
+            }
+            rule_count += 1
+
+        rules_data["rule_group_" + rule_group.get("id")] = r
+    return rules_data
+
+def stitch_grid(images, blank=False):
+    grid = []
+    for i in range(3):
+        row = []
+        for j in range(3):
+            if (i == 2) & (j == 2) & (blank == True): 
+                blank = np.ones(images[0].shape)*255
+                blank = np.pad(blank, 6, constant_values=255)
+                row += [blank]
+                continue
+            im = np.pad(images[i*3+j], 2)
+            im = np.pad(im, 4, constant_values=255)
+            row += [im]
+        row = np.concatenate(row, axis=1)
+        grid += [row]
+    grid = np.concatenate(grid, axis=0)
+    grid = np.pad(grid, 2, mode='constant', constant_values=255)
+    return grid
+
+if __name__ == "__main__":
+    main() 
