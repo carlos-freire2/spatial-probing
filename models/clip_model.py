549a0b3bf1d3bcd62ef8c7a66ff6594b26defebb
Madeleine Fenner <madeleinefenner@mac.mynetworksettings.com>
2025-12-07 14:36:17 -0500

    feat: update models, support GPU inference

--- models/clip_model.py
+++ models/clip_model.py
@@ -1,5 +1,5 @@
 import torch
-from transformers import AutoImageProcessor, CLIPVisionModel
+from transformers import AutoImageProcessor, CLIPModel
 
 class CLIPWrapper(torch.nn.Module):
     def __init__(self, model_name: str = ""):
@@ -9,5 +9,16 @@ class CLIPWrapper(torch.nn.Module):
 
     def forward(self, pixel_values, output_hidden_states):
         return self.model(pixel_values=pixel_values, output_hidden_states=output_hidden_states)
+    
+    def get_hidden_states(self, x: list[torch.Tensor], device) -> list[torch.Tensor]:
+        inputs = self.processor(images=x, return_tensors="pt")
+
+        self.model = self.model.to(device)
+        self.model.eval()
+        inputs = inputs.to(device)
         
+        with torch.inference_mode():
+            outputs = self.model(**inputs, output_hidden_states=True)
+        all_hidden_states = outputs.hidden_states
+        return all_hidden_states
         
--- models/dino_model.py
+++ models/dino_model.py
@@ -10,9 +10,14 @@ class DINOv3Wrapper(torch.nn.Module):
     def forward(self, pixel_values, output_hidden_states):
         return self.model(pixel_values=pixel_values, output_hidden_states=output_hidden_states)
     
-    def get_hidden_states(self, x: list[torch.Tensor]) -> list[torch.Tensor]:
+    def get_hidden_states(self, x: list[torch.Tensor], device) -> list[torch.Tensor]:
         inputs = self.processor(images=x, return_tensors="pt")
+                
+        self.model = self.model.to(device)
+        self.model.eval()
+        inputs = inputs.to(device)
+
         with torch.inference_mode():
             outputs = self.model(**inputs)
         all_hidden_states = outputs.hidden_states
-        return all_hidden_states 
+        return all_hidden_states
