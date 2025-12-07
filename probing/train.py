c5d050e45734e8ff03bfcd6472f72f6d097cb9ae
Madeleine Fenner <madeleinefenner@mac.mynetworksettings.com>
2025-12-07 11:52:16 -0500

    fix: update training script inputs

--- probing/train.py
+++ probing/train.py
@@ -1,7 +1,12 @@
+import torch.nn as nn
+import torch
+
 def train_and_evaluate(
     model: nn.Module, 
     train_data: torch.Tensor, 
     train_labels: torch.Tensor, 
+    val_data: torch.Tensor, 
+    val_labels: torch.Tensor, 
     test_data: torch.Tensor, 
     test_labels: torch.Tensor,
     lr: int,
@@ -10,6 +15,8 @@ def train_and_evaluate(
     ) -> Tuple[dict, nn.Module]:
 
     # TODO implement train loop
+    # TODO implement val loop
     # TODO implement test loop
+    # metrics {} could have acc and loss during training, final acc based on test set
     #return metrics, trained_model
     raise NotImplementedError() 
